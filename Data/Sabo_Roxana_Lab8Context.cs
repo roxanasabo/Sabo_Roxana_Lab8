8518dd97225bf4048444d49b0b0536a9925d4548
Roxana Camelia Sabo <Sabo.Gh.Roxana@student.utcluj.ro>
2021-11-25 17:22:16 +0200

    Add project files.

---
+++ Data/Sabo_Roxana_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Sabo_Roxana_Lab8.Models;
+
+namespace Sabo_Roxana_Lab8.Data
+{
+    public class Sabo_Roxana_Lab8Context : DbContext
+    {
+        public Sabo_Roxana_Lab8Context (DbContextOptions<Sabo_Roxana_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Sabo_Roxana_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Sabo_Roxana_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Sabo_Roxana_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Sabo_Roxana_Lab8.Data;
+using Sabo_Roxana_Lab8.Models;
+
+namespace Sabo_Roxana_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Sabo_Roxana_Lab8.Data.Sabo_Roxana_Lab8Context _context;
+
+        public IndexModel(Sabo_Roxana_Lab8.Data.Sabo_Roxana_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book
+                .Include(b => b.Publisher)
+                .ToListAsync();
+        }
+    }
+}
