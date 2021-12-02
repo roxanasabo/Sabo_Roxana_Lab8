fb67d66e2f8ca31257820226d3101bf30ef11988
Roxana Camelia Sabo <Sabo.Gh.Roxana@student.utcluj.ro>
2021-12-02 15:04:59 +0200

    Am implementat laboratorul 9.

---
+++ Models/AssignedCategoryData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sabo_Roxana_Lab8.Models
+{
+    public class AssignedCategoryData
+    {
+
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public bool Assigned { get; set; }
+    }
+}
